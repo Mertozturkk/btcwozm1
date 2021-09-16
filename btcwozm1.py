a95bd06face26db46c69256dc162bb95a9311b6d
Mert Öztürk <42963332+Mertozturkk@users.noreply.github.com>
2021-09-16 21:30:02 +0300

    Add files via upload

--- btcwozm1.py
+++ btcwozm1.py
@@ -34,40 +34,40 @@ except:
     print("An unexpected error has occurred!")
     LOGGER.info("An error occurred while requesting")
 
-def hint_hunter():
+def data_scraping():
     """
     This function performs data extraction, listing data, saving in JSON format
     and printing the desired data to the screen.
     """
-    hint_set1 = []
-    hint_set2 = []
+    HİNT_LIST = []
+    NUMBER_LIST = []
     text_ = soup.find_all("span",attrs={"class":"Clue-text--3lZl7"})
     # we access the relevant tags through the link we use
     try:
         for link in text_:
-            hint_set1.append((link.text))
-        # using a list method We add it to our list called hint_set1
+            HİNT_LIST.append((link.text))
+        # using a list method We add it to our list called HİNT_LIST
 
         num_ = soup.find_all("span",attrs={"class":"Clue-label--2IdMY"}) # same process for the numbers
         for link in num_:
-            hint_set2.append((link.text))
+            NUMBER_LIST.append((link.text))
         LOGGER.info('Content gathered')
     except:
         LOGGER.info('Error occurred while accessing data')
 
-    json_ = {hint_set1[i]: hint_set2[i] for i in range(len(hint_set1))}
+    APPNAME = {HİNT_LIST[i]: NUMBER_LIST[i] for i in range(len(HİNT_LIST))}
     # we create a dictionary from the obtained lists,
 
-    with open("hint.json","a") as file:
-        json.dump(json_, file) # open an JSON file and save it
+    with open("APPNAME.json","a") as file:
+        json.dump(APPNAME, file) # open an JSON file and save it
     LOGGER.info('JSON file created,')
 
     print('=== Across ===')
-    for i in range(len(hint_set1)): # a small loop to print hints on the screen
-        print('{}. {}'.format(hint_set2[i],hint_set1[i]))
+    for i in range(len(HİNT_LIST)): # a small loop to print hints on the screen
+        print('{}. {}'.format(NUMBER_LIST[i],HİNT_LIST[i]))
         if i == 4 :
             print("=== Down ===")
     LOGGER.info('Data is saved and printed on the screen')
 
 if __name__== "__main__":
-    hint_hunter()
+    data_scraping()
